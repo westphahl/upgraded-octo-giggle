1dbacebd70be8c67edb98356b1c0b49031d3385a
James E. Blair <jeblair@redhat.com>
2019-02-09 08:35:46 -0800

    Add restclient-cpp

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,4 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+apt-get update
+apt-get install debian-archive-keyring curl gnupg apt-transport-https
+curl -L https://packagecloud.io/mrtazz/restclient-cpp/gpgkey | apt-key add -
+cat > /etc/apt/sources.list.d/mrtazz_restclient-cpp.list <<EOF
+deb https://packagecloud.io/mrtazz/restclient-cpp/debian/ buster main
+deb-src https://packagecloud.io/mrtazz/restclient-cpp/debian/ buster main
+EOF
+apt-get update
+apt-get install restclient-cpp
+
 autoreconf -fi
